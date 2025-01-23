8f22464200d32852663402ad73e4fd2039ef2096
Md. Emon Miah <md.emonmiahisthebest@gmail.com>
2025-01-23 09:53:57 +0600

    update first deploy

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p)qe5*1rmw9w!=(r=ka!pa_!nkz#h$cdqaq#h(0t%$0oo*p=ug
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
